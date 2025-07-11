dcfb3d3fa79dafe4568a7adb72712f5a8eacf534
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2025-07-11 18:29:24 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -55,7 +55,7 @@ const RegisterotherRoles = async (req, res) => {
       path.join(__dirname, "../Mailtemplate/setPasswordTemplate.html"),
       "utf-8"
     );
-    const link = `${process.env.CLIENT_URL}/set-password/${email}`;
+    const link = `${process.env.CLIENT_URL}/carete-user/${email}`;
     console.log("link", link);
     const mailContent = html
       .replace("{{name}}", name)
